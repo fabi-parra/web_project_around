26f98c9d7112a6ee97c3e401448f376eb12fb6c4
FabiolaParraV <fabiolaparravejar@gmail.com>
2024-06-29 17:04:42 -0400

    webpack config

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -2,8 +2,6 @@ export default class FormValidator {
   constructor(config, formElement) {
     this._config = config;
     this._formElement = formElement;
-    console.log(config);
-    console.log(formElement);
   }
 
   _setEventListeners() {
