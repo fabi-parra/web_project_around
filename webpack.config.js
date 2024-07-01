c1d0c7c69fedeecbf66abeb1071dddc600749034
FabiolaParraV <fabiolaparravejar@gmail.com>
2024-07-01 16:57:32 -0400

    update project

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,37 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   entry: {
-    main: "./src/scripts/index.js",
+    main: './src/index.js'
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-    publicPath: "",
-    clean: true,
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: ''
   },
-  target: ["web", "es5"],
+  target: ['web', 'es5'],
   stats: { children: true },
-  mode: "development",
+  mode: 'development',
   devServer: {
-    static: path.resolve(__dirname, "dist"),
+    static: path.resolve(__dirname, './dist'),
     compress: true,
     port: 8080,
-    open: true,
+    open: true
   },
   module: {
-    rules: [
+    rules: [ // esto es un array de reglas
+      // añádele un objeto que contenga reglas para Babel
       {
+        // una expresión regular que busca todos los archivos js
         test: /\.js$/,
+        // todos los archivos deben ser procesados por babel-loader
         loader: "babel-loader",
-        exclude: "/node_modules/",
+        // excluye la carpeta node_modules, no necesitamos procesar archivos en ella
+        exclude: "/node_modules/"
       },
       {
         test: /\.css$/,
@@ -36,20 +39,25 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
+            options: {
+              importLoaders: 1
+            }
           },
+          "postcss-loader"
         ],
       },
       {
+        // añade la regla para el procesamiento de archivos
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: "asset/resource",
+        type: "asset/resource"
       },
-    ],
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: "./src/index.html" // ruta a nuestro archivo index.html
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
-  ],
-};
+    new MiniCssExtractPlugin() // conecta el plugin para fusionar archivos CSS
+  ]
+}
