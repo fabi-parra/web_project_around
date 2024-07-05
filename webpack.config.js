28558fe47d75849ab7b5065cc4d3c29e030b9c08
FabiolaParraV <fabiolaparravejar@gmail.com>
2024-07-05 14:41:40 -0400

    fix git ignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,30 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   entry: {
-    main: './src/page/index.js'
+    main: "./src/page/index.js",
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: ''
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
   },
-  target: ['web', 'es5'],
+  target: ["web", "es5"],
   stats: { children: true },
-  mode: 'development',
+  mode: "development",
   devServer: {
-    static: path.resolve(__dirname, './dist'),
+    static: path.resolve(__dirname, "./dist"),
     compress: true,
     port: 8080,
-    open: true
+    open: true,
   },
   module: {
-    rules: [ // esto es un array de reglas
+    rules: [
+      // esto es un array de reglas
       // añádele un objeto que contenga reglas para Babel
       {
         // una expresión regular que busca todos los archivos js
@@ -31,7 +32,7 @@ module.exports = {
         // todos los archivos deben ser procesados por babel-loader
         loader: "babel-loader",
         // excluye la carpeta node_modules, no necesitamos procesar archivos en ella
-        exclude: "/node_modules/"
+        exclude: "/node_modules/",
       },
       {
         test: /\.css$/,
@@ -40,24 +41,24 @@ module.exports = {
           {
             loader: "css-loader",
             options: {
-              importLoaders: 1
-            }
+              importLoaders: 1,
+            },
           },
-          "postcss-loader"
+          "postcss-loader",
         ],
       },
       {
         // añade la regla para el procesamiento de archivos
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: "asset/resource"
+        type: "asset/resource",
       },
-    ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html" // ruta a nuestro archivo index.html
+      template: "./src/index.html", // ruta a nuestro archivo index.html
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin() // conecta el plugin para fusionar archivos CSS
-  ]
-}
+    new MiniCssExtractPlugin(), // conecta el plugin para fusionar archivos CSS
+  ],
+};
